eb5fa9eddbd113363d2a28ebec3552be66c11661
mrfabroa <eric.fabroa@ycdsbk12.ca>
2017-11-08 15:35:05 -0500

    Transffered from Amy's fork

---
+++ src/org/usfirst/frc/team4001/robot/commands/ArcadeDrive.java
@@ -0,0 +1,48 @@
+package org.usfirst.frc.team4001.robot.commands;
+
+import org.usfirst.frc.team4001.robot.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+import org.usfirst.frc.team4001.*;
+
+
+/**
+ *
+ */
+public class ArcadeDrive extends Command {
+
+    public ArcadeDrive() {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    	requires(Robot.drivetrain);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    }
+ 
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	double moveForward = Robot.oi.getLeftAxis();
+    	double turn = Robot.oi.getRightAxis();
+
+    	
+    	Robot.drivetrain.arcadeDrive(moveForward, turn);
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    	Robot.drivetrain.stop();
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
